5d40dceb1bd08ff247a17ec45e90b773d5d0a984
mowenpark <michaelowenpark@gmail.com>
2016-06-07 15:54:25 -0700

    heroku push

--- app/assets/javascripts/vis/donutCtrl.js
+++ app/assets/javascripts/vis/donutCtrl.js
@@ -1,8 +1,8 @@
 angular.module('visOne')
 
-.controller('DonutChartController', function($scope, $window) {
+.controller('DonutChartController', [ '$scope', '$window', function($scope, $window) {
   $scope.charts = d3.range(9).map(function () {
-    return d3.range(10).map(Math.random)
-  })
+      return d3.range(10).map(Math.random)
+    })
   angular.element($window).on('resize', function(){ $scope.$digest() })
-})
+}])
