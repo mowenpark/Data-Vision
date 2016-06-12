fbbb9c90d2dc3465d9efc18688685ae5ebaedac2
mowenpark <michaelowenpark@gmail.com>
2016-06-11 21:18:15 -0700

    add inicident selection

--- app/controllers/api/incidents_controller.rb
+++ app/controllers/api/incidents_controller.rb
@@ -1,9 +1,10 @@
 class Api::IncidentsController < ApplicationController
 
   def index
-    @incidents = Incident.where('extract(year  from date_time) = ?', 2015).limit(100)
+    @incidents = Incident.where(category: "WARRANTS").where('extract(year  from date_time) = ?', 2015)
   end
 
   def show
   end
+
 end
--- app/views/api/incidents/index.json.jbuilder
+++ app/views/api/incidents/index.json.jbuilder
@@ -1,3 +1,12 @@
+hsh = Hash.new { |hash, key| hash[key] = {} }
+7.times do |i|
+  hsh[i] = Hash.new { |hash, key| hash[key] = 0 }
+  24.times do |j|
+    hsh[i][j] = 0
+  end
+end
+
+
 json.array! @incidents.each do |incident|
   json.id incident.id
   json.category incident.category
