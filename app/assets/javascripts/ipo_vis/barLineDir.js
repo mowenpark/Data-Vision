140eb5b4b7ed3345c21514c67f028eeca88d20c6
mowenpark <michaelowenpark@gmail.com>
2016-06-26 14:26:04 -0700

    add x axis

--- app/assets/javascripts/ipo_vis/barLineDir.js
+++ app/assets/javascripts/ipo_vis/barLineDir.js
@@ -27,134 +27,162 @@ angular.module('visOne')
           middle: 50
       }
 
+      var timeX = d3.scale.linear();
+
+      var dateStart = data[0].year;
+      var dateEnd = data[data.length - 1].year;
+
+      timeX.domain([dateStart,dateEnd]);
+      timeX.range([0,lineWidth-visPadding.middle]);
+
+      var chartColors = d3.scale.ordinal();
+      chartColors
+          .domain(['corr', 'disp', 'hfu', 'hfri', 'fof', 'date'])
+          .range(['#FC9E27', '#3A7FA3', '#B5CF6B', '#D6616B', '#E7BA52', '#888']);
+
+      var interactiveX = d3.scale.ordinal();
+      interactiveX.domain(d3.range(0,data.length,1));
+      interactiveX.rangeBands([0,lineWidth - visPadding.middle])
+
+      var maxD = d3.max(data, function(d) { return d.biotech_ipos; });
+      var maxC = d3.max(data, function(d) { return d.other_ipos; });
+      var maxDC = d3.max([maxD, maxC]);
+
+      var minD = d3.min(data, function(d) { return d.biotech_ipos; });
+      var minC = d3.min(data, function(d) { return d.other_ipos; });
+      var minDC = d3.min([minD, minC]);
+
+      var lineY = d3.scale.linear();
+      lineY.domain([minDC < 0 ? minDC : 0, maxDC]);
+      lineY.range([lineHeight,0]);
+
+      var maxFOF = d3.max(data, function(d) { return d.fof; });
+      var maxHFRI = d3.max(data, function(d) { return d.hfri; });
+      var maxHFU = d3.max(data, function(d) { return d.hfu; });
+
+
+      var minFOF = d3.min(data, function(d) { return d.fof; });
+      var minHFRI = d3.min(data, function(d) { return d.hfri; });
+      var minHFU = d3.min(data, function(d) { return d.hfu; });
+
+
+      var fofBound = Math.max(Math.abs(minFOF), Math.abs(maxFOF));
+      var hfriBound = Math.max(Math.abs(minHFRI), Math.abs(maxHFRI));
+      var hfuBound = Math.max(Math.abs(minHFU), Math.abs(maxHFU));
+
+      var fofY = d3.scale.linear();
+      fofY.domain([-fofBound, 0, fofBound])
+      fofY.range([lineHeight/2, lineHeight/2, 0]);
+
+      var hfriY = d3.scale.linear();
+      hfriY.domain([-hfriBound, 0, hfriBound])
+      hfriY.range([lineHeight/2, lineHeight/2, 0]);
+
+      var hfuY = d3.scale.linear();
+      hfuY.domain([-hfuBound, 0, hfuBound])
+      hfuY.range([lineHeight/2, lineHeight/2, 0]);
+
       function getColor(d) {
            if(d.key) return axes.chartColors(d.key);
            else return axes.chartColors(d);
        }
 
-      function buildChart(data) {
-          axes = new Axes(data);
-
-          container = d3.select(el).selectAll('g.container').data(data);
-          container.enter().append('g')
-              .attr('class', 'container')
-              .attr('transform', 'translate(' + visPadding.left + ',' + visPadding.top + ')');
-
-          // var skeleton = buildSkeleton(data);
-          // var disCorrLines = buildDisCorr(data);
-          // var alphaBars = buildAlpha(data);
-          // var scatter = buildScatter(data);
-          // var text = buildTextBox(data);
-          // var interactiveLayer = buildInteractiveLayer(data);
-          // var tooltips = setupTooltips(scatter, interactiveLayer, text, data);
-      }
-
       function buildSkeleton(data) {
 
           var xAxis = d3.svg.axis()
-              .scale(axes.timeX)
+              .scale(timeX)
               .orient('bottom');
 
-          container.append('g')
-              .attr('class', 'x-axis')
-              .attr('transform', 'translate(0,' + (axes.charts('corrDisp') + axes.lineY(0)) + ')')
-              .call(xAxis);
+          d3.select('g.container').append('g')
+            .attr('class', 'x-axis')
+            .call(xAxis);
       }
 
+      function buildDisCorr(data) {
 
-      function Axes(data) {
+        var dcWrap = d3.select('g.container').selectAll('g.d-c-wrap')
+            .append('g')
+            .attr('class', 'd-c-wrap');
 
-        this.chartColors = d3.scale.ordinal();
-        this.chartColors
-            .domain(['corr', 'disp', 'hfu', 'hfri', 'fof', 'date'])
-            .range(['#FC9E27', '#3A7FA3', '#B5CF6B', '#D6616B', '#E7BA52', '#888']);
+        var dcText = dcWrap.append('text')
+            .attr('transform', 'translate(-11,' + lineHeight/2 + ')')
+            .attr('dy', '.3em')
+            .attr('text-anchor', 'end');
 
-        this.timeX = d3.time.scale();
+        dcText.append('tspan')
+            .text('Biotech vs.');
+        dcText.append('tspan')
+            .text('Other Ipos')
+            .attr('x', '0')
+            .attr('y', '1.4em');
 
-        debugger
+        var keys = ['corr','disp'];
+        var linesWrap = dcWrap.selectAll('g').data(function(d) {
+            var a = keys.map(function(k) {
+                return {
+                    data: d,
+                    key: k
+                };
+            })
+            return a;
+        });
+        linesWrap.enter().append('g').attr('class', 'lines-wrap');
 
-        var dateStart = new Date(data[0].date);
-        var dateEnd = new Date(data[data.length - 1].date);
+        var line = d3.svg.line()
+            .x(function(d) { return timeX(d.year); });
 
-        this.timeX.domain([dateStart,dateEnd]);
-        this.timeX.range([0,lineWidth-visPadding.middle]);
+        var linesPath = linesWrap.selectAll('path').data(function(d,i) {
+            keys[i] = d.key
+            return [d.data];
+        });
 
-        this.interactiveX = d3.scale.ordinal();
-        this.interactiveX.domain(d3.range(0,data.length,1));
-        this.interactiveX.rangeBands([0,lineWidth - visPadding.middle])
+        linesPath.enter().append('path')
+              .attr('d', function(d,i,j) {
+                  var k = keys[j];
+                  line.y(function(d) { return lineY(d[k]); })
+                  return line(d,i);
+              })
+              .attr('stroke', function(d,i,j) { return getColor(keys[j]) })
+              .attr('stroke-width', 2)
+              .attr('fill', 'none');
 
-        var maxD = d3.max(data, function(d) { return d.disp; });
-        var maxC = d3.max(data, function(d) { return d.corr; });
-        var maxDC = d3.max([maxD, maxC]);
+          dcWrap.append('g')
+              .attr('class', 'z-line');
 
-        var minD = d3.min(data, function(d) { return d.disp; });
-        var minC = d3.min(data, function(d) { return d.corr; });
-        var minDC = d3.min([minD, minC]);
+          var zLine = dcWrap.select('.z-line');
 
-        this.lineY = d3.scale.linear();
-        this.lineY.domain([minDC < 0 ? minDC : 0, maxDC]);
-        this.lineY.range([lineHeight,0]);
+          zLine.append('text')
+              .text(0)
+              .attr('x', lineWidth - visPadding.middle)
+              .attr('y', lineY(0))
+              .attr('dy', '0.33em')
+              .attr('dx', '1em')
+              .attr('text-anchor', 'end');
 
-        var maxFOF = d3.max(data, function(d) { return d.fof; });
-        var maxHFRI = d3.max(data, function(d) { return d.hfri; });
-        var maxHFU = d3.max(data, function(d) { return d.hfu; });
+          return dcWrap;
+      }
 
+      scope.$watch('data', function(data) {
 
-        var minFOF = d3.min(data, function(d) { return d.fof; });
-        var minHFRI = d3.min(data, function(d) { return d.hfri; });
-        var minHFU = d3.min(data, function(d) { return d.hfu; });
+        var svg = d3.select(el).append('svg')
+          .attr("width", chartWidth)
+          .attr("height", chartHeight)
 
+        var container = d3.select('svg').append('g')
+            .attr('class', 'container')
+            .attr('transform', 'translate(' + visPadding.left + ',' + visPadding.top + ')');
 
-        var fofBound = Math.max(Math.abs(minFOF), Math.abs(maxFOF));
-        var hfriBound = Math.max(Math.abs(minHFRI), Math.abs(maxHFRI));
-        var hfuBound = Math.max(Math.abs(minHFU), Math.abs(maxHFU));
+        var skeleton = buildSkeleton(data);
+        var disCorrLines = buildDisCorr(data);
+        // var alphaBars = buildAlpha(data);
+        // var scatter = buildScatter(data);
+        // var text = buildTextBox(data);
+        // var interactiveLayer = buildInteractiveLayer(data);
+        // var tooltips = setupTooltips(scatter, interactiveLayer, text, data);
 
-        this.fofY = d3.scale.linear();
-        this.fofY.domain([-fofBound, 0, fofBound])
-        this.fofY.range([lineHeight/2, lineHeight/2, 0]);
+    });
 
-        this.hfriY = d3.scale.linear();
-        this.hfriY.domain([-hfriBound, 0, hfriBound])
-        this.hfriY.range([lineHeight/2, lineHeight/2, 0]);
-
-        this.hfuY = d3.scale.linear();
-        this.hfuY.domain([-hfuBound, 0, hfuBound])
-        this.hfuY.range([lineHeight/2, lineHeight/2, 0]);
-
-        //For scatter only
-        var maxFH = d3.max([maxFOF,maxHFU]);
-        var minFH = d3.min([minFOF,minHFU]);
-
-        var alphaBound = Math.max(Math.abs(minFH), Math.abs(maxFH));
-        this.alphaY = d3.scale.linear();
-        this.alphaY.domain([-alphaBound, 0,alphaBound]);
-        this.alphaY.range([12, 2, 12]);
-
-        this.quadrants = {};
-        this.quadrants.hfu = d3.scale.linear();
-        this.quadrants.fof = d3.scale.linear();
-
-        this.scatterX = d3.scale.linear();
-        this.scatterY = d3.scale.linear();
-
-        var medD = d3.median(data, function(d) { return d.disp; });
-        var medC = d3.median(data, function(d) { return d.corr; });
-        this.scatterX.domain([minC, medC, maxC]);
-        this.scatterY.domain([minD, medD, maxD]);
-
-        this.scatterX.range([0, scatterWidth/2, scatterWidth]);
-        this.scatterY.range([scatterHeight, scatterHeight/2, 0]);
-
-        var cDHeight = 3*(lineHeight + chartPadding);
-        this.charts = d3.scale.ordinal();
-        this.charts
-            .domain(['corr','disp','corrDisp', 'hfri', 'hfu', 'fof'])
-            .range([3*(lineHeight + chartPadding), 3*(lineHeight + chartPadding) + 13, 3*(lineHeight + chartPadding), 2*(lineHeight + chartPadding), (lineHeight + chartPadding), 0]);
-
-        };
-
-    };
-
+  }
 
     return {
       link: link,
