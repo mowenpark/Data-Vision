5449078996eb6bc738b62ec2ae1a15e0a5791c4c
mowenpark <michaelowenpark@gmail.com>
2016-06-26 21:02:40 -0700

    finish barline interactive

--- app/assets/javascripts/ipo_vis/barLineDir.js
+++ app/assets/javascripts/ipo_vis/barLineDir.js
@@ -16,7 +16,7 @@ angular.module('visOne')
           lineWidth = 600;
 
       var chartPadding = 30,
-          chartHeight = 490,
+          chartHeight = 390,
           chartWidth = 907;
 
       var visPadding = {
@@ -26,6 +26,8 @@ angular.module('visOne')
           left: 70,
           middle: 50
       }
+
+      var scatterWidth = scatterHeight = chartWidth - lineWidth - visPadding.left - visPadding.right;
 
       var keyReplace = {
         biotech_percent: 'Tech & Biotech',
@@ -43,8 +45,8 @@ angular.module('visOne')
 
       var chartColors = d3.scale.ordinal();
       chartColors
-          .domain(['biotech_ipos', 'other_ipos', 'hfu', 'hfri', 'fof', 'date'])
-          .range(['#FC9E27', '#3A7FA3', '#B5CF6B', '#D6616B', '#E7BA52', '#888']);
+          .domain(['biotech_ipos', 'other_ipos', 'biotech_percent', 'other_percent', 'fof', 'date'])
+          .range(['#FC9E27', '#3A7FA3', '#FC9E27', '#3A7FA3', '#E7BA52', '#888']);
 
       var interactiveX = d3.scale.ordinal();
       interactiveX.domain(d3.range(0,data.length,1));
@@ -80,8 +82,8 @@ angular.module('visOne')
       other_percentY.range([lineHeight/2, lineHeight/2, 0]);
 
       var charts = d3.scale.ordinal();
-      charts.domain(['biotech_ipos','other_ipos','corrDisp', 'biotech_percent', 'other_percent', 'fof'])
-        .range([3*(lineHeight + chartPadding), 3*(lineHeight + chartPadding) + 13, 3*(lineHeight + chartPadding), 2*(lineHeight + chartPadding), (lineHeight + chartPadding), 0]);
+      charts.domain(['biotech_ipos','other_ipos', 'biotech_percent', 'other_percent'])
+        .range([2*(lineHeight + chartPadding), 2*(lineHeight + chartPadding) + 13, (lineHeight + chartPadding), 0]);
 
       function getColor(d) {
            if(d.key) return chartColors(d.key);
@@ -231,6 +233,168 @@ angular.module('visOne')
               .attr('fill', 'none');
       }
 
+      function buildTextBox(data) {
+        var tWrap = d3.select('g.container').append('g')
+            .attr('class', 'text-wrap')
+            .attr('transform', 'translate(' + (lineWidth + visPadding.middle - visPadding.left) + ',' + (scatterHeight + visPadding.top) + ')');
+
+        var labels = ['Year', 'Tech', 'Tech % Profitable', 'Other', 'Other % Profitable'];
+        var keys = ['date', 'biotech_ipos', 'biotech_percent', 'other_ipos', 'other_percent'];
+        var o = labels.map(function(d,i) {
+            return { label: d, key: keys[i] };
+        });
+
+        var textScale = d3.scale.ordinal();
+        textScale.domain(o.map(function(d) { return d.key }))
+            .range(d3.range(0,81,27))
+
+        var ttLabel = tWrap.selectAll('text.tt-label').data(o);
+        ttLabel.enter().append('text')
+            .attr('class', function(d) { return 'tt-label ' + d.key })
+            .attr('x', function(d,i) { return i < 3 ? 18 : scatterWidth/2 + 20 })
+            .attr('y', function(d,i) { return textScale(d.key) + i%3 * 27 })
+            .attr('fill', getColor)
+            .text(function(d) { return d.label });
+
+        var ttVal = tWrap.selectAll('text.tt-value').data(o);
+        ttVal.enter().append('text')
+            .attr('class', function(d) { return 'tt-value ' + d.key })
+            .attr('y', function(d,i) { return textScale(d.key) + (i%3 * 27) + 18 })
+            .attr('x', function(d,i) { return i < 3 ? 18 : scatterWidth/2 + 20 })
+            .attr('text-anchor', 'start')
+            .text('-');
+
+        return ttVal;
+
+      };
+
+
+      function setupTooltips(line, text, data) {
+
+        var line = line;
+
+        dispatch.on('iTooltip', function(d,i,ext) {
+            var x = d3.event.layerX - visPadding.left;
+            var location = 0;
+            var point;
+
+            if(!d.point) {
+                var leftEdges = interactiveX.range();
+                var width = interactiveX.rangeBand();
+                while(x > (leftEdges[location] + width)) {
+                    location++
+                }
+                location = Math.min(++location,d.length-1)
+                point = d[location];
+            }
+
+            if(d.point) {
+                var dDate = d.point.year;
+
+                line.transition().duration(300)
+                    .attr('opacity',1)
+                    .attr('transform', 'translate(' + (timeX(dDate) + 2) + ',0)');
+
+            } else {
+                if(!line.classed('active')) {
+                    line.transition().duration(200)
+                        .attr('opacity',1);
+                }
+
+                line
+                    .classed('active', true)
+                    .attr('transform', 'translate(' + (x + 2) + ',0)');
+            }
+
+            showText(point,i);
+
+        });
+
+        dispatch.on('hideTooltip', function(d,i,ext) {
+
+            text.text('-')
+
+            line.classed('active', false)
+                .transition().duration(200)
+                .attr('opacity',0);
+        });
+
+        function showText(o) {
+            line.selectAll('text').text(function(d) {
+                var num = o[d];
+                if(num === null) { return 'N/A'; }
+                else { return num; }
+            });
+
+            text.text(function(d) {
+                    if(d.key == 'date') {
+                        var retDate = o['year'];
+                        return retDate;
+                    } else if (d.key.indexOf('percent') == -1) {
+                        var retVal = o[d.key];
+
+                        if(o[d.key] === null) { return 'N/A'; }
+                        else { return retVal; }
+                    } else {
+                      var retVal = o[d.key];
+
+                      if(o[d.key] === null) { return 'N/A'; }
+                      else { return retVal + '%'; }
+                    }
+
+                })
+                .attr('fill', function(d) { return o[d.key] < 0 ? '#D6616B' : '#000' });
+        }
+
+      }
+
+      function buildInteractiveLayer(data) {
+        var iWrap = d3.select('g.container').selectAll('g.interactive-wrap').data([data]);
+        iWrap.enter().append('g').attr('class', 'interactive-wrap');
+
+        var lineWrap = iWrap.append('g')
+            .attr('class', 'line-wrap')
+            .attr('opacity', 0);
+
+        lineWrap.append('line')
+            .attr('y1', 0)
+            .attr('y2', chartHeight)
+            .attr('x1', 0)
+            .attr('x2', 0)
+            .attr('dx', 1)
+            .attr('stroke', '#ccc')
+            .attr('stroke-width', 2);
+
+        var valBoxes = lineWrap.selectAll('rect.text-box').data(['biotech_ipos','biotech_percent','other_percent','other_ipos','disp']);
+
+        valBoxes.enter().append('rect')
+            .attr('class',function(d) { return 'text-box ' + d })
+            .attr('x',2)
+            .attr('y', charts)
+            .attr('fill', '#fff')
+            .attr('height', function(d) { return d == 'corrDisp' ? 32 : 16 })
+            .attr('width', 25);
+
+        valBoxes.enter().append('text')
+            .attr('x',4)
+            .attr('y', function(d) { return charts(d) + 11 })
+            .attr('fill', getColor)
+            .text('')
+
+        iWrap.append('rect')
+            .attr('width', lineWidth - visPadding.middle)
+            .attr('height', chartHeight)
+            .attr('class', 'interactive-layer')
+            .attr('fill', '#000')
+            .attr('opacity', 0)
+            .on('mouseover', dispatch.iTooltip)
+            .on('mousemove', dispatch.iTooltip)
+            .on('mouseout', dispatch.hideTooltip);
+
+        return lineWrap;
+
+      }
+
       scope.$watch('data', function(data) {
 
         var svg = d3.select(el).append('svg')
@@ -245,10 +409,9 @@ angular.module('visOne')
         var skeleton = buildSkeleton(data);
         var disCorrLines = buildDisCorr(data);
         var alphaBars = buildAlpha(data);
-        // var scatter = buildScatter(data);
-        // var text = buildTextBox(data);
-        // var interactiveLayer = buildInteractiveLayer(data);
-        // var tooltips = setupTooltips(scatter, interactiveLayer, text, data);
+        var text = buildTextBox(data);
+        var interactiveLayer = buildInteractiveLayer(data);
+        var tooltips = setupTooltips(interactiveLayer, text, data);
 
     });
 
